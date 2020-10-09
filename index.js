7e1269100a0ca2355438a97e9dcd28ab22fc23fc
Alex <alexandre.javet@hotmail.com>
2020-10-09 12:10:42 +0200

    [ADD] /c command sends every items in citation.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {Client, MessageEmbed, Collection} = require('discord.js')
 const {token} = require('./secrets.json')
+const {citations} = require('./citation.json')
 
 const client = new Client();
 
@@ -8,8 +9,10 @@ client.once('ready', () => {
 });
 
 client.on("message", async message => {
-    if (message.content.toLowerCase() == "/citations"){
-      
+    if (message.content.toLowerCase() == "/c"){
+      for (var i = 0; i < citations.length; i++) {
+        const msg = await message.channel.send(`"${citations[i].Text}" - *${citations[i].Author}*`)
+      }
     }
 });
 
