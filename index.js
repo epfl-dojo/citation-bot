304d2873e05777c58580f8ae8806467d6884bcb7
Alex <alexandre.javet@hotmail.com>
2020-10-09 11:35:35 +0200

    [ADD] Fixed the bot init, added a onmessage command

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const {client, MessageEmbed, Collection} = require('discord.js')
+const {Client, MessageEmbed, Collection} = require('discord.js')
 const {token} = require('./secrets.json')
+
+const client = new Client();
+
+client.once('ready', () => {
+  console.info('✨ The bot is running.');
+});
+
+client.on("message", async message => {
+    const msg = await message.channel.send(`test`);
+});
 
 
 client.login(token)
